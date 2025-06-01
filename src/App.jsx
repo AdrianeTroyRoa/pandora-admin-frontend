c52bd6013199e6e2c2c0fd57d7333cd73dbcd250
Troy Roa <roatroy850@gmail.com>
2025-06-01 08:08:19 +0800

    feat: searchbar and add button

--- src/App.jsx
+++ src/App.jsx
@@ -75,35 +75,73 @@ function App() {
           PRODUCTS
         </div>
       </div>
-      {/*cards for products*/}
-      <div className="flex flex-col justify-center items-center gap-3">
-        {productSample.map((product) => (
-          <div className="border-1 border-gray-200 grid grid-cols-3 gap-7 items-center p-4 w-256 rounded">
-            <div className="flex items-center">
-              <div className="border-1 p-6">Img</div>
-            </div>
-            <label className="flex flex-col text-justify">
-              <span className="text-lg font-bold">{product.title}</span>
-              <span className="text-sm text-gray-500">
-                {product.description}
-              </span>
-            </label>
-            <div className="grid gap-3 justify-end">
+
+      <div className="flex flex-col items-center gap-7">
+        <div className="flex flex-row items-center justify-center gap-7">
+          {/*Search index*/}
+          <div className="w-full max-w-sm min-w-[200px]">
+            <div className="relative">
+              <input
+                className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                placeholder="Search products..."
+              />
               <button
-                className="hover:text-white text-red-700 bg-white border-1 border-red-700
-                     hover:bg-red-700 p-1 rounded"
+                className="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
               >
-                Delete
-              </button>
-              <button
-                className="hover:text-white border-1 border-blue-700 hover:bg-blue-700
-                     text-blue-700 p-1 rounded"
-              >
-                Edit
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4 mr-2"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                Search
               </button>
             </div>
           </div>
-        ))}
+
+          {/*Add Product button*/}
+          <button className="bg-blue-700 p-2 rounded text-white text-xs hover:bg-blue-800 min-w-[120px]">
+            + Add Product
+          </button>
+        </div>
+
+        {/*cards for products*/}
+        <div className="flex flex-col justify-center items-center gap-3">
+          {productSample.map((product) => (
+            <div className="border-1 border-gray-200 grid grid-cols-3 gap-7 items-center p-4 lg:w-256 sm:w-128 rounded">
+              <div className="flex items-center">
+                <div className="border-1 p-6">Img</div>
+              </div>
+              <label className="flex flex-col text-justify">
+                <span className="text-lg font-bold">{product.title}</span>
+                <span className="text-sm text-gray-500">
+                  {product.description}
+                </span>
+              </label>
+              <div className="grid gap-3 justify-end">
+                <button
+                  className="hover:text-white text-red-700 bg-white border-1 border-red-700
+                     hover:bg-red-700 p-1 rounded"
+                >
+                  Delete
+                </button>
+                <button
+                  className="hover:text-white border-1 border-blue-700 hover:bg-blue-700
+                     text-blue-700 p-1 rounded"
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
