58dc63b48fc71ef2443e591fc28a6d327b331e36
Troy Roa <roatroy850@gmail.com>
2025-06-01 08:44:26 +0800

    chore: style in upload image

--- src/App.jsx
+++ src/App.jsx
@@ -141,8 +141,15 @@ function App() {
 
                   <div className="flex flex-col gap-4 p-6">
                     <div className="w-full max-w-sm min-w-[200px]">
-                      <input type="file" />
+                      <label className="block mb-2 text-sm text-slate-600">
+                        Upload product image
+                      </label>
+                      <input
+                        className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                        type="file"
+                      />
                     </div>
+                    <hr />
                     <div className="w-full max-w-sm min-w-[200px]">
                       <input
                         type="text"
